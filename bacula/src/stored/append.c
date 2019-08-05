0aabfddab2cfa4ba8bc09cb99bcc0f290515a2e7
Krzysztof Lis <klis33@gmail.com>
2019-08-05 14:27:42 +0200

    Fix logging

--- bacula/src/stored/append.c
+++ bacula/src/stored/append.c
@@ -28,6 +28,148 @@
 #define KLDEBUG_FILE_IDX 1
 
 
+static const char *KL_stream_to_ascii(int stream)
+{
+   static char buf[20];
+
+   switch (stream & STREAMMASK_TYPE) {
+   case STREAM_UNIX_ATTRIBUTES:
+      return _("Unix attributes");
+   case STREAM_FILE_DATA:
+      return _("File data");
+   case STREAM_MD5_DIGEST:
+      return _("MD5 digest");
+   case STREAM_GZIP_DATA:
+      return _("GZIP data");
+   case STREAM_COMPRESSED_DATA:
+      return _("Compressed data");
+   case STREAM_UNIX_ATTRIBUTES_EX:
+      return _("Extended attributes");
+   case STREAM_SPARSE_DATA:
+      return _("Sparse data");
+   case STREAM_SPARSE_GZIP_DATA:
+      return _("GZIP sparse data");
+   case STREAM_SPARSE_COMPRESSED_DATA:
+      return _("Compressed sparse data");
+   case STREAM_PROGRAM_NAMES:
+      return _("Program names");
+   case STREAM_PROGRAM_DATA:
+      return _("Program data");
+   case STREAM_SHA1_DIGEST:
+      return _("SHA1 digest");
+   case STREAM_WIN32_DATA:
+      return _("Win32 data");
+   case STREAM_WIN32_GZIP_DATA:
+      return _("Win32 GZIP data");
+   case STREAM_WIN32_COMPRESSED_DATA:
+      return _("Win32 compressed data");
+   case STREAM_MACOS_FORK_DATA:
+      return _("MacOS Fork data");
+   case STREAM_HFSPLUS_ATTRIBUTES:
+      return _("HFS+ attribs");
+   case STREAM_UNIX_ACCESS_ACL:
+      return _("Standard Unix ACL attribs");
+   case STREAM_UNIX_DEFAULT_ACL:
+      return _("Default Unix ACL attribs");
+   case STREAM_SHA256_DIGEST:
+      return _("SHA256 digest");
+   case STREAM_SHA512_DIGEST:
+      return _("SHA512 digest");
+   case STREAM_SIGNED_DIGEST:
+      return _("Signed digest");
+   case STREAM_ENCRYPTED_FILE_DATA:
+      return _("Encrypted File data");
+   case STREAM_ENCRYPTED_WIN32_DATA:
+      return _("Encrypted Win32 data");
+   case STREAM_ENCRYPTED_SESSION_DATA:
+      return _("Encrypted session data");
+   case STREAM_ENCRYPTED_FILE_GZIP_DATA:
+      return _("Encrypted GZIP data");
+   case STREAM_ENCRYPTED_FILE_COMPRESSED_DATA:
+      return _("Encrypted compressed data");
+   case STREAM_ENCRYPTED_WIN32_GZIP_DATA:
+      return _("Encrypted Win32 GZIP data");
+   case STREAM_ENCRYPTED_WIN32_COMPRESSED_DATA:
+      return _("Encrypted Win32 Compressed data");
+   case STREAM_ENCRYPTED_MACOS_FORK_DATA:
+      return _("Encrypted MacOS fork data");
+   case STREAM_PLUGIN_NAME:
+      return _("Plugin Name");
+   case STREAM_PLUGIN_DATA:
+      return _("Plugin Data");
+   case STREAM_RESTORE_OBJECT:
+      return _("Restore Object");
+   case STREAM_ACL_AIX_TEXT:
+      return _("AIX Specific ACL attribs");
+   case STREAM_ACL_DARWIN_ACCESS_ACL:
+      return _("Darwin Specific ACL attribs");
+   case STREAM_ACL_FREEBSD_DEFAULT_ACL:
+      return _("FreeBSD Specific Default ACL attribs");
+   case STREAM_ACL_FREEBSD_ACCESS_ACL:
+      return _("FreeBSD Specific Access ACL attribs");
+   case STREAM_ACL_HPUX_ACL_ENTRY:
+      return _("HPUX Specific ACL attribs");
+   case STREAM_ACL_IRIX_DEFAULT_ACL:
+      return _("Irix Specific Default ACL attribs");
+   case STREAM_ACL_IRIX_ACCESS_ACL:
+      return _("Irix Specific Access ACL attribs");
+   case STREAM_ACL_LINUX_DEFAULT_ACL:
+      return _("Linux Specific Default ACL attribs");
+   case STREAM_ACL_LINUX_ACCESS_ACL:
+      return _("Linux Specific Access ACL attribs");
+   case STREAM_ACL_TRU64_DEFAULT_ACL:
+      return _("TRU64 Specific Default ACL attribs");
+   case STREAM_ACL_TRU64_ACCESS_ACL:
+      return _("TRU64 Specific Access ACL attribs");
+   case STREAM_ACL_SOLARIS_ACLENT:
+      return _("Solaris Specific POSIX ACL attribs");
+   case STREAM_ACL_SOLARIS_ACE:
+      return _("Solaris Specific NFSv4/ZFS ACL attribs");
+   case STREAM_ACL_AFS_TEXT:
+      return _("AFS Specific ACL attribs");
+   case STREAM_ACL_AIX_AIXC:
+      return _("AIX Specific POSIX ACL attribs");
+   case STREAM_ACL_AIX_NFS4:
+      return _("AIX Specific NFSv4 ACL attribs");
+   case STREAM_ACL_FREEBSD_NFS4_ACL:
+      return _("FreeBSD Specific NFSv4/ZFS ACL attribs");
+   case STREAM_ACL_HURD_DEFAULT_ACL:
+      return _("GNU Hurd Specific Default ACL attribs");
+   case STREAM_ACL_HURD_ACCESS_ACL:
+      return _("GNU Hurd Specific Access ACL attribs");
+   case STREAM_XATTR_HURD:
+      return _("GNU Hurd Specific Extended attribs");
+   case STREAM_XATTR_IRIX:
+      return _("IRIX Specific Extended attribs");
+   case STREAM_XATTR_TRU64:
+      return _("TRU64 Specific Extended attribs");
+   case STREAM_XATTR_AIX:
+      return _("AIX Specific Extended attribs");
+   case STREAM_XATTR_OPENBSD:
+      return _("OpenBSD Specific Extended attribs");
+   case STREAM_XATTR_SOLARIS_SYS:
+      return _("Solaris Specific Extensible attribs or System Extended attribs");
+   case STREAM_XATTR_SOLARIS:
+      return _("Solaris Specific Extended attribs");
+   case STREAM_XATTR_DARWIN:
+      return _("Darwin Specific Extended attribs");
+   case STREAM_XATTR_FREEBSD:
+      return _("FreeBSD Specific Extended attribs");
+   case STREAM_XATTR_LINUX:
+      return _("Linux Specific Extended attribs");
+   case STREAM_XATTR_NETBSD:
+      return _("NetBSD Specific Extended attribs");
+   default:
+      sprintf(buf, "%d", stream);
+      return (const char *)buf;
+   }
+}
+
+
+
+
+
+
 
 /* Responses sent to the File daemon */
 static char OK_data[]    = "3000 OK data\n";
@@ -242,7 +384,7 @@ bool do_append_data(JCR *jcr)
       total_stream_len += stream_len;
 
       Pmsg6(50, ">>>> APPEND Files=%d FilInx=%d stream=%d stream_len=%lld tot_stream_len=%lld %s\n",
-    	jcr->JobFiles, file_index, stream, stream_len, total_stream_len, stream_to_ascii(stream));
+    	jcr->JobFiles, file_index, stream, stream_len, total_stream_len, KL_stream_to_ascii(stream));
 #endif
 
 
